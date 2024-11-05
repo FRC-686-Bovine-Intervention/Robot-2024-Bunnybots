edf5d8191d9f916eb7ff18648830fc71eea477e7
Shanmukha Pothukuchi <shanmukha.pothukuchi@gmail.com>
2024-11-05 12:45:55 -0500

    Update advantage kit

--- src/main/java/frc/robot/subsystems/drive/ModuleIOSim.java
+++ src/main/java/frc/robot/subsystems/drive/ModuleIOSim.java
@@ -5,6 +5,7 @@ import static edu.wpi.first.units.Units.Volts;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.units.measure.MutAngle;
 import edu.wpi.first.units.measure.MutVoltage;
 import edu.wpi.first.units.measure.Voltage;
@@ -13,8 +14,14 @@ import frc.robot.Robot;
 
 public class ModuleIOSim implements ModuleIO {
     // jKg constants unknown, stolen from Mechanical Advnatage
-    private final FlywheelSim driveSim = new FlywheelSim(DCMotor.getFalcon500(1), DriveConstants.driveWheelGearReduction, 0.025);
-    private final FlywheelSim turnSim  = new FlywheelSim(DCMotor.getFalcon500(1), DriveConstants.turnWheelGearReduction,  0.004);
+    private final FlywheelSim driveSim = new FlywheelSim(
+        LinearSystemId.createFlywheelSystem(DCMotor.getFalcon500(1), 0.025, DriveConstants.driveWheelGearReduction),
+        DCMotor.getFalcon500(1)
+    );
+    private final FlywheelSim turnSim = new FlywheelSim(
+        LinearSystemId.createFlywheelSystem(DCMotor.getFalcon500(1), 0.004, DriveConstants.turnWheelGearReduction),
+        DCMotor.getFalcon500(1)
+    );
 
     private final MutAngle turnRelativePosition = Radians.mutable(0);
     private final MutAngle turnAbsolutePosition = Radians.mutable(Math.random() * 2.0 * Math.PI);
