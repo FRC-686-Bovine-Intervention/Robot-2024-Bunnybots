c098784931a545090a8db9e1fe9c8e0f633a7449
Michael Sims <michael@the-sims-family.net>
2024-12-03 22:12:40 -0500

    Rio ports

--- src/main/java/frc/util/led/strips/hardware/AddressableStrip.java
+++ src/main/java/frc/util/led/strips/hardware/AddressableStrip.java
@@ -8,14 +8,18 @@ import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
-import frc.util.hardwareID.RIOPort;
+import frc.util.hardwareID.rioPorts.PWMPort;
 
 public class AddressableStrip implements HardwareStrip {
     private final AddressableLED strip;
     private final AddressableLEDBuffer buffer;
 
-    public AddressableStrip(RIOPort port, int length) {
-        this.strip = port.addressableLED();
+    public AddressableStrip(PWMPort port, int length) {
+        this(port.addressableLED(), length);
+    }
+
+    public AddressableStrip(AddressableLED leds, int length) {
+        this.strip = leds;
         this.buffer = new AddressableLEDBuffer(length);
         this.strip.setLength(this.buffer.getLength());
         this.strip.setData(this.buffer);
